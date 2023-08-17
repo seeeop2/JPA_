7b355803c9c56c8d69d10660f1e9e68bb755a077
seeeop2 <seeeop2@gmail.com>
2023-08-17 21:44:13 +0900

    데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,10 +1,19 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class Member {
+
+    @Id
+    private long id;
+//    @Column(name = "USERNAME") DB 컬럼명 지정할 수 있다.
+    @Column(unique = true, length = 10)  //컬럼에 unique 제약조건 추가, 길이는 10자 까지
+    private String name;
+    private int age;
+//    private int gogo2;
 
     //JPA는 생성자 만들 때, 기본 생성자도 만들어줘야한다.
     public Member() {
@@ -15,9 +24,6 @@ public class Member {
         this.name = name;
     }
 
-    @Id
-    private long id;
-    private String name;
 
     public long getId() {
         return id;
